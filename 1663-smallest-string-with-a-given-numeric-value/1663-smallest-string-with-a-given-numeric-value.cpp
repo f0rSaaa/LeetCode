d1610112d3d0d0111eada86decabd84204ccddff
Ankit Sagar <sagarankit13@gmail.com>
2022-03-22 09:17:30 +0530

    Time: 79 ms (32.17%), Space: 27.2 MB (15.32%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    
+    //https://leetcode.com/problems/smallest-string-with-a-given-numeric-value/discuss/1871662/JavaC%2B%2B-Easiest-Possible-Exaplained!!
     string getSmallestString(int n, int k) {
         string s = "";
         for(int i=0;i<n;i++){
