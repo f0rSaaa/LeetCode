8b65268c41a0d7250f9d3500c3f8cd412dcfd37b
Ankit Sagar <sagarankit13@gmail.com>
2022-03-24 09:15:34 +0530

    Time: 108 ms (52.57%), Space: 41.8 MB (91.53%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
-        sort(people.rbegin(),people.rend());  //sort in reverse
+        // sort(people.rbegin(),people.rend());  //sort in reverse
+        // int i=0;
+        // int j=people.size()-1;
+        // while(i<=j){
+        //     if(people[i]+people[j]<=limit) //sabse patla + sabse mota ek saath jaa sakte hai kya wo check kar rahe hai
+        //         j--;
+        //     ++i;
+        // }
+        // return i;
+        sort(people.begin(),people.end());
         int i=0;
         int j=people.size()-1;
+        int boats = 0;
         while(i<=j){
-            if(people[i]+people[j]<=limit) //sabse patla + sabse mota ek saath jaa sakte hai kya wo check kar rahe hai
-                j--;
-            ++i;
+            if(people[i]+people[j]<=limit)
+                ++i;
+            --j;
+            ++boats;
         }
-        return i;
+        return boats;
             
     }
 };
