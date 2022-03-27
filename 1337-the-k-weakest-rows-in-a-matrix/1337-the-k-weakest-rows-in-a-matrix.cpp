63b5108628cf468215446f585356fe8804d0e5ad
Ankit Sagar <sagarankit13@gmail.com>
2022-03-27 20:27:05 +0530

    Time: 27 ms (13.19%), Space: 10.9 MB (7.93%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
+    int binary(vector<int> m) {
+        int l = 0;
+        int h = m.size()-1;
+        while(l <= h)
+        {
+            int mid = l + (h-l)/2;
+            if(m[mid] == 0)
+                h = mid - 1;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
+    
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
         unordered_map<int, int>mpp;
         vector<int>ans;
         int n = mat.size();
         int m = mat[0].size();
         for(int i=0;i<n;i++){
-            int count = 0;
-            for(int j=0;j<m;j++){
-                if(mat[i][j] == 1)
-                    count++;
-            }
+            int count = binary(mat[i]);
+            // int count = 0;
+            // for(int j=0;j<m;j++){
+            //     if(mat[i][j] == 1)
+            //         count++;
+            // }
             mpp[i] = count;
         }
         // for(auto it:mpp){
