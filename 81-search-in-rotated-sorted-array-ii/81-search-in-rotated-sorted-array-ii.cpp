8b71c332d54f049a980c087f8742032a61d6378a
Ankit Sagar <sagarankit13@gmail.com>
2022-03-28 11:14:02 +0530

    Time: 15 ms (19.28%), Space: 13.8 MB (99.53%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -3,7 +3,7 @@ public:
     bool search(vector<int>& nums, int target) {
         int start = 0;
         int end = nums.size() -1;
-        while(start <=end){
+        while(start <= end){
             int mid =  start + (end-start)/2;
             if(nums[mid] == target)
                 return true;
