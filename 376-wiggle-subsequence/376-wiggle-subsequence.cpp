a74b214b9ed445f4be778030487db8c0c8a58fee
Ankit Sagar <sagarankit13@gmail.com>
2022-04-02 10:59:39 +0530

    Time: 7 ms (35.31%), Space: 7.2 MB (42.96%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -8,27 +8,11 @@ public:
             int dif = nums[i+1] - nums[i];
             v.push_back(dif);
         }
-        // for(auto it:v){
-        //     cout << it << " ";
-        // }
-        // cout << endl;
         int i=0;
         int count = 1;
-        // int currdiff;
         int prevdiff = 0;
         while(i<v.size()){
-            //heap buffer overflow
-            // if(v[i]>0 && v[i+1]<0){
-            //     count++;
-            //     i++;
-            // } else if(v[i]<0 && v[i+1]>0){
-            //     count++;
-            //     i++;
-            // } else if((v[i] >0 && v[i+1] > 0) || (v[i] <0 && v[i+1] < 0)){
-            //     while((v[i] >0 && v[i+1] > 0) || (v[i] <0 && v[i+1] < 0)){
-            //         i++;
-            //     }
-            // }
+
             if((v[i]>0 && prevdiff<=0)||(v[i]<0 && prevdiff>=0)){
                 count++;
                 prevdiff = v[i];
