2f957d4235d5e2042ec03f70c1aa9ea309e86a76
Ankit Sagar <sagarankit13@gmail.com>
2022-04-08 15:10:44 +0530

    Time: 6 ms (69.22%), Space: 7.3 MB (89.76%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+ 
+    bool isPalindrome(string s) {
+        int i=0;;
+        int j = s.length() - 1;
+        while(i<j){
+            while(isalnum(s[i]) == false && i<j) i++;
+            while(isalnum(s[j]) == false && i<j) j--;
+            if(tolower(s[i]) != tolower(s[j])) return false;
+            i++;
+            j--;
+        }
+        return true;
+        
+
+    }
+};
