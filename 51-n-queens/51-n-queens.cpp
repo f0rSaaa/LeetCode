21bee5f6df774b6490628993ae5589f2824ec691
Ankit Sagar <sagarankit13@gmail.com>
2022-04-16 16:55:28 +0530

    Time: 7 ms (73.26%), Space: 7.2 MB (68.07%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,53 +1,26 @@
 class Solution {
 public:
     
-    
-    bool issafe(int row, int col, vector<string>&board, int n){
-        int duprow = row;
-        int dupcol = col;
-        while(row>=0 && col >=0){   //checking if there is a queen in the topleft direction if there then this position is not safe
-            if(board[row][col] == 'Q')
-                return false;
-            row--;
-            col--;
-        }
-        
-        row = duprow;
-        col = dupcol;
-        while(col>=0){
-            //checking if there is a queen in the left direction if there is a queen in the left direction then it is not safe to place a queen there
-            if(board[row][col] == 'Q'){
-                return false;
-            }
-            col--;
-        }
-        
-        row = duprow;
-        col = dupcol;
-        while(row<n && col >=0){ //checking if there is a queen in the bottom right direction if there is a queen there then it is not safe to place a queen there
-            if(board[row][col] == 'Q')
-                return false;
-            row++;
-            col--;
-            
-        }
-        return true;
-    }
-    
-    void solve(int col, vector<vector<string>>&ans, vector<string>&board, int n){
+    void solve(int col, vector<vector<string>>&res, vector<string>&board, int n, vector<int>&leftRow, vector<int>&upperDiag, vector<int>&lowerDiag){
         if(col == n){
-            ans.push_back(board);
+            res.push_back(board);
             return;
         }
-        for(int row = 0;row<n;row++){
-            if(issafe(row, col,board, n)){
+        for(int row = 0; row<n;row++){
+            if(leftRow[row] == 0 && upperDiag[(n-1)+(col-row)] == 0 && lowerDiag[row+col] == 0){
+                leftRow[row] = 1;
+                upperDiag[(n-1)+(col-row)] = 1;
+                lowerDiag[row+col] = 1;
                 board[row][col] = 'Q';
-                solve(col+1,ans,board, n);
+                solve(col+1,res,board,n,leftRow, upperDiag,lowerDiag);
+                leftRow[row] = 0;
+                upperDiag[(n-1)+(col-row)] = 0;
+                lowerDiag[row+col] = 0;
                 board[row][col] = '.';
             }
         }
     }
-    
+
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>>res;
         vector<string>board(n);
@@ -55,7 +28,8 @@ public:
         for(int i=0;i<n;i++){
             board[i] = s;
         }
-        solve(0,res,board,n);
+        vector<int>leftRow(n,0),upperDiag(2*n-1,0),lowerDiag(2*n-1,0);
+        solve(0,res,board,n,leftRow,upperDiag,lowerDiag);
         return res;
     }
 };
