cedec170cd042119355c1b9db65e52418ffa2f54
Ankit Sagar <sagarankit13@gmail.com>
2022-05-20 16:47:43 +0530

    Time: 15 ms (77.02%), Space: 17.3 MB (9.86%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,18 +1,36 @@
 class Solution {
 public:
+    
+    int lb(vector<int>dp, int x){
+        int start = 0;
+        int end = dp.size() -1;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(dp[mid]<x){
+                start = mid+1;
+            } else {
+                end = mid -1;
+            }
+        }
+        return start;
+    }
+    
     int lengthOfLIS(vector<int>& nums) {
+        int len = 1;
+        vector<int>dp;
         int n = nums.size();
-        vector<vector<int>>dp(n+1, vector<int>(n+1,0));
+        dp.push_back(nums[0]);
+        for(int i=1;i<n;i++){
+            if(nums[i]> dp.back()){
+                dp.push_back(nums[i]);
+                len++;
+            } else {
+    			int ind = lb(dp,nums[i]);
+                // int ind = lower_bound(dp.begin(),dp.end(),nums[i]) - dp.begin();
+                dp[ind] = nums[i];
+            }
+        }
 
-    	for(int ind=n-1;ind>=0;ind--){
-    		for(int pre = ind-1;pre>=-1;pre--){
-    			int len = 0 + dp[ind+1][pre+1];
-    			if(pre == -1 || nums[ind]>nums[pre])
-    				len = max(len, 1 + dp[ind+1][ind+1]);
-
-    			dp[ind][pre+1] = len;
-    		}
-    	}
-        return dp[0][0];
+        return len;
     }
 };
