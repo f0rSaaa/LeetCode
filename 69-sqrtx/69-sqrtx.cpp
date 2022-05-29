296abe070835af9b81d47f10360fd09bb29a021e
Ankit Sagar <sagarankit13@gmail.com>
2022-05-29 20:09:38 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (74.61%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -2,11 +2,28 @@ class Solution {
 public:
     int mySqrt(int x) {
         // return pow(x,0.5);
-        long r = x;
-        while(r*r >  x){
-            r = (r + x/r) / 2;
+        //nweton method
+        // long r = x;
+        // while(r*r >  x){
+        //     r = (r + x/r) / 2;
+        // }
+        // return r;
+        //binary search
+        long long start = 0;
+        long long end = INT_MAX;
+        long long ans = 0;
+        while(start <= end){
+            long long mid = start + (end-start)/2;
+            if(mid * mid == x)
+                return mid;
+            else if(mid * mid > x)
+                end =  mid -1;
+            else{
+                ans = mid;
+                start = mid+1;    
+            }
         }
-        return r;
+        return ans;
     }
     
 };
