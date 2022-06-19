0862d438482a4f91d420ca7fd332165f9cf7427f
Ankit Sagar <sagarankit13@gmail.com>
2022-06-19 11:23:47 +0530

    Time: 1038 ms (33.33%), Space: 264.8 MB (66.67%) - LeetHub

--- 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -1,45 +1,25 @@
 class Solution {
 public:
-//     int solve(int idx, int size, int sum , string s, vector<vector<int>>&dp){
-//         if(idx<0)
-//             return 0;
-        
-//         if(dp[idx][size]!= -1)
-//             return dp[idx][size];
-        
-//         int notpick = solve(idx-1, size,sum, s,dp);
-//         int pick = 0;
-//         if((sum-(s[idx]-'0')*pow(2,size))>=0)
-//             pick = 1 +solve(idx-1, size+1,(sum-(s[idx]-'0')*pow(2,size)), s,dp);
-        
-//         return dp[idx][size] = max(pick, notpick);
-//     }
-    
-//     int longestSubsequence(string s, int k) {
-//         int  n = s.length();
-//         vector<vector<int>>dp(n, vector<int>(n,-1));
-//         return solve(n-1,0,k,s,dp);
-//     }
-    
-    int f(int i,int size, int sum, string &s, vector<vector<int>>&dp){
-        if(i<0){
+    int solve(int idx, int size, int sum , string &s, vector<vector<int>>&dp){
+        if(idx<0)
             return 0;
-        }
-        if(dp[i][size] != -1){
-            return dp[i][size];
-        }
-        int no = f(i-1,size,sum,s,dp);
-        int yes = 0;
-        if((sum-(s[i]-'0')*pow(2,size)) >=0){
-            yes = 1+ f(i-1,size+1,(sum-(s[i]-'0')*pow(2,size)),s,dp);
-        }
-        return dp[i][size]=max(no,yes);
+        
+        if(dp[idx][size]!= -1)
+            return dp[idx][size];
+        
+        int notpick = solve(idx-1, size,sum, s,dp);
+        int pick = 0;
+        if((sum-(s[idx]-'0')*pow(2,size))>=0)
+            pick = 1 +solve(idx-1, size+1,(sum-(s[idx]-'0')*pow(2,size)), s,dp);
+        
+        return dp[idx][size] = max(pick, notpick);
     }
+    
     int longestSubsequence(string s, int k) {
-        
-        int n = s.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return f(n-1,0,k,s,dp);
-        
+        int  n = s.length();
+        vector<vector<int>>dp(n, vector<int>(n,-1));
+        return solve(n-1,0,k,s,dp);
     }
+    
+    
 };
