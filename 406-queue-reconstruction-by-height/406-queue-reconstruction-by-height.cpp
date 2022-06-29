fba2bedca5169aa0a0860690233cc8c6ea93232f
Ankit Sagar <sagarankit13@gmail.com>
2022-06-29 13:19:55 +0530

    Time: 255 ms (29.99%), Space: 25.1 MB (7.63%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    
-    static bool compare(vector<int> a, vector<int>  b){
-        if(a[0] == b[0]) return a[1] < b[1];
-        else return a[0] < b[0];
-    }
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
-        sort(people.begin(), people.end(), compare);
-        vector<vector<int>> ans(people.size(),vector<int>(2, -1));
-        for(int i = 0; i < people.size(); i++){
-            int count = 0;
-            for(int j = 0; j < ans.size(); j++){
-                if(count == people[i][1] and ans[j][0] == -1){
-                    ans[j] = people[i];
-                    break;
-                }
-                else if(ans[j][0] == -1 or ans[j][0] == people[i][0]) count++;
-            }
+        sort(people.begin(), people.end(),[&](vector<int>a, vector<int>b){
+            if(a[0]==b[0])
+                return a[1]<b[1];
+            else
+                return a[0]>b[0];
+        });
+        int n = people.size();
+        vector<vector<int>>res;
+        for(auto it:people){
+            res.insert(res.begin()+it[1],it);
         }
-        return ans;
-        
+      
+        return res;    
     }
 };
