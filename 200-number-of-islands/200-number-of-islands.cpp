05f35112287afbf6e093f1fd712398c63ae8ba4e
Ankit Sagar <sagarankit13@gmail.com>
2022-07-01 16:05:27 +0530

    Time: 56 ms (44.35%), Space: 13.3 MB (31.45%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     // vector<pair<int, int>>d = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
-    vector<pair<int, int>>d = {{1,0},{0,1},{1,0},{0,-1}};
+    vector<pair<int, int>>d = {{1,0},{0,1},{-1,0},{0,-1}};
     
     void dfs(vector<vector<char>>& grid, vector<vector<int>>&vis, int i, int j, int n, int m){
         if(i <0 || j < 0 || i >=n || j >=m || grid[i][j] == '0')
@@ -10,15 +10,15 @@ public:
             return;
         vis[i][j] = 1;
         // dfs(gird, vis, )
-        // for(int k=0;i<4;i++){
-        //     int x = i+d[k].first;
-        //     int y = j+d[k].second;
-        //     dfs(grid, vis,i+x,i+y,n,m);
-        // }
-        dfs(grid, vis, i-1, j, n, m);
-        dfs(grid, vis, i+1, j, n, m);
-        dfs(grid, vis, i, j-1, n, m);
-        dfs(grid, vis, i, j+1, n, m);
+        for(int k=0;k<4;k++){
+            int x = i+d[k].first;
+            int y = j+d[k].second;
+            dfs(grid, vis,x,y,n,m);
+        }
+        // dfs(grid, vis, i-1, j, n, m);
+        // dfs(grid, vis, i+1, j, n, m);
+        // dfs(grid, vis, i, j-1, n, m);
+        // dfs(grid, vis, i, j+1, n, m);
     }
     
     int numIslands(vector<vector<char>>& grid) {
