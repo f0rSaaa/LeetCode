b1251535d25f365a057bb5a42228e0fdeb9fc05a
Ankit Sagar <sagarankit13@gmail.com>
2022-07-01 23:15:25 +0530

    Time: 94 ms (76.08%), Space: 30 MB (70.80%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -6,19 +6,19 @@ public:
         queue<pair<int, int>>qu;
         int n = mat.size();
         int m = mat[0].size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for(int i=0;i<n;++i){
+            for(int j=0;j<m;++j){
                 if(mat[i][j] == 0){
                     qu.push({i,j});
                 } else 
-                    mat[i][j] = -1; // marking the cell as not visited and is yet to be processed
+                    mat[i][j] = -1; // marking the cell as not visited and is yet to be processed or take a visited array and do this in it instead 
             }
         }
         while(!qu.empty()){
             int x = qu.front().first;
             int y = qu.front().second;
             qu.pop();
-            for(int i=0;i<4;i++){
+            for(int i=0;i<4;++i){
                 int nx = x + dir[i].first;
                 int ny = y + dir[i].second;
                 if(nx < 0 || ny < 0 || nx >= n || ny >=m || mat[nx][ny] != -1)
