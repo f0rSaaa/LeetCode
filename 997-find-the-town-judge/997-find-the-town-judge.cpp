004d0977395e0eeb7ba35bfa32a86c980a9257be
Ankit Sagar <sagarankit13@gmail.com>
2022-07-03 18:28:18 +0530

    Time: 371 ms (15.05%), Space: 60.7 MB (94.80%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int>indeg(n+1,0);
-        vector<int>outdeg(n+1,0);
-        for(auto it:trust){
-            indeg[it[1]]++;
-            outdeg[it[0]]++;
+        // vector<int>indeg(n+1,0);
+        // vector<int>outdeg(n+1,0);
+        // for(auto &it:trust){
+        //     indeg[it[1]]++;
+        //     outdeg[it[0]]++;
+        // }
+        // for(int i=1;i<=n;i++){
+        //     if(outdeg[i]==0 && indeg[i] == n-1){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+        vector<int>count(n+1,0);
+        for(auto &it:trust){
+            count[it[0]]--;
+            count[it[1]]++;
         }
         for(int i=1;i<=n;i++){
-            if(outdeg[i]==0 && indeg[i] == n-1){
+            if(count[i] == n-1)
                 return i;
-            }
         }
         return -1;
     }
