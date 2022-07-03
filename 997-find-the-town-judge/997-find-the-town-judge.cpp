cf0b39ea7a7e163368be276481027284578e0926
Ankit Sagar <sagarankit13@gmail.com>
2022-07-03 18:28:33 +0530

    Time: 256 ms (54.10%), Space: 60.6 MB (99.16%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -18,7 +18,7 @@ public:
             count[it[0]]--;
             count[it[1]]++;
         }
-        for(int i=1;i<=n;i++){
+        for(int i=1;i<=n;++i){
             if(count[i] == n-1)
                 return i;
         }
