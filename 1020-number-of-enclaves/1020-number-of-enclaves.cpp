125f60bd3904c2e4565e6327ca59d19241942823
Ankit Sagar <sagarankit13@gmail.com>
2022-07-04 21:54:08 +0530

    Time: 75 ms (75.01%), Space: 21.7 MB (56.29%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
+
+    vector<pair<int, int>>dir = {{1,0},{0,1},{-1,0},{0,-1}};
+    
     int dfs(vector<vector<int>>&grid, int i, int j, int n, int m){
         if(i<0 || j <0 || i>=n || j>=m || grid[i][j] == 0)
             return 0;
         grid[i][j] = 0;
         
         return 1 + dfs(grid,i-1,j,n,m) + dfs(grid,i,j-1,n,m)+ dfs(grid,i+1,j,n,m) + dfs(grid,i,j+1,n,m);
+
     }
     
     int numEnclaves(vector<vector<int>>& grid) {
@@ -20,7 +24,7 @@ public:
         }
         int region1 = 0;
         for(int i=0;i<n;++i){
-            for(int j=0;j<m;j++){
+            for(int j=0;j<m;++j){
                 if((i == 0 || j == 0 || i == n-1 || j == m-1) && grid[i][j] == 1)
                     region1+=dfs(grid,i,j,n,m);
             }
