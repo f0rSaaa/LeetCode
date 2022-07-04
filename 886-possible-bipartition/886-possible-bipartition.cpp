910ec9427316b957f2601ab5d5265d45374acc83
Ankit Sagar <sagarankit13@gmail.com>
2022-07-04 17:44:51 +0530

    Time: 433 ms (16.54%), Space: 86.6 MB (5.14%) - LeetHub

--- 886-possible-bipartition/886-possible-bipartition.cpp
+++ 886-possible-bipartition/886-possible-bipartition.cpp
@@ -1,26 +1,7 @@
 class Solution {
 public:
     
-    // bool isbipa(vector<int>adj[], vector<int>&color, int node){
-    //     queue<int>qu;
-    //     qu.push(node);
-    //     color[node] = 1;
-    //     while(!qu.empty()){
-    //         int x = qu.front();
-    //         qu.pop();
-    //         for(auto it:adj[x]){
-    //             if(color[x] == color[it])
-    //                 return false;
-    //             if(color[it] == -1){
-    //                 // int newColor = 1- color[x];      
-    //                 color[it] = 1- color[x];
-    //                 qu.push(it);
-    //             }
-    //         }
-    //     }
-    //     return true;
-    // }
-    
+
     bool possibleBipartition(int n, vector<vector<int>>& dis) {
         vector<int>adj[n+1];
         for(auto it:dis){
@@ -28,13 +9,7 @@ public:
             adj[it[1]].push_back(it[0]);
         }
         unordered_map <int, int> color;
-        // vector<int>color(n+1,-1);
-        // for(int i=1;i<=n;++i){
-        //     if(color[i] == -1)
-        //         if(!isbipa(adj, color,i));
-        //             return false;              
-        // }
-        for(int i = 1; i <= n; i++) {
+        for(int i = 1; i <= n; ++i) {
             queue<int> q;
             q.push(i);
             color.insert({i, 0});
@@ -43,14 +18,17 @@ public:
                 int node = q.front();
                 q.pop();
                 
-                for(auto itr = adj[node].begin(); itr != adj[node].end(); itr++) {
-                    // all neighbors of current node are given alternate color
-                    int altColor = color[node]^1;
-                    if(color.find(*itr) == color.end()) color.insert({*itr, altColor});
-                    else if(color[*itr] == altColor)    continue;
-                    else    return false;
+                for(auto &it:adj[node]){
+                    int newColor = color[node] ^ 1;
+                    // int newColor = 1 - color[node];
+                    if(color.find(it) == color.end()){
+                        color[it] = newColor;
+                    } else if( color[it] == newColor)
+                        continue;
+                    else
+                        return false;
                     
-                    q.push(*itr);
+                    q.push(it);
                 }
             }
         }
