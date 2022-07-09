76dabceb0c4783863b5647e19e4bb4adec9f5432
Ankit Sagar <sagarankit13@gmail.com>
2022-07-10 00:12:35 +0530

    Time: 823 ms (12.69%), Space: 49.2 MB (14.90%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -5,7 +5,7 @@ public:
         if(idx==n-1) return true;
         if(idx>=n) return false;
         if(!dp[idx]) return dp[idx];
-        for(int i=1;i<=nums[idx];i++){
+        for(int i=1;i<=nums[idx];++i){
             if(solve(nums,n,idx+i)) return dp[idx] = true;
         }
         return dp[idx] = false;
