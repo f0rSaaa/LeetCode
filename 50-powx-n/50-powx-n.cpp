982dba6ba35d39c90b5bb6ea0c52f2066cf6911d
Ankit Sagar <sagarankit13@gmail.com>
2022-07-12 00:52:30 +0530

    Time: 4 ms (32.18%), Space: 6 MB (43.35%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     double myPow(double x, int n) {
         long N = abs(n);
-        double res = 1;
-        while (N > 0) {
-            if(N % 2 ==0){  //not for negatvive values
-                x *= x;
-                N /= 2;
+        double res= 1;
+        while(N){
+            if(N%2 == 0){
+                x *=x;
+                N/=2;
             } else {
-                res *=x;
+                res*=x;
                 N--;
             }
         }
-        cout << res << endl;
-        return n>=0?res:1/res;
+        if(n < 0)     //if res is negative then return reciprocal of it
+            return 1/res;
+        return res;
     }
 };
