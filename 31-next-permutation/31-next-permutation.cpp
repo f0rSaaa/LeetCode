035416da0aeacc6f38c137d429b8adffb0e7a7fd
Ankit Sagar <sagarankit13@gmail.com>
2022-07-11 09:32:27 +0530

    Time: 11 ms (24.76%), Space: 12.1 MB (36.55%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n= nums.size(), k, l;
-        for(k = n - 2; k>=0;k--){
-            if(nums[k]< nums[k+1])
+        int n = nums.size();
+        int k,i;
+        for(k = n-2;k>=0;k--){
+            //from second last elemnet find elem which is less than the previus elem from right to left
+            // 1 2 3 here it will be 2 as 2 < 3
+            if(nums[k] < nums[k+1])
                 break;
         }
-        if(k < 0){
-            reverse(nums.begin(), nums.end());
-        } else {
-            for(l = n -1; l>k;l--){
-                if(nums[l]>nums[k])
-                    break;
+        if(k < 0){ // the array was in descending order
+            reverse(nums.begin(),nums.end());
+        } else {    //find the first elem which is greater than the element at kth index from right side
+            for(i = n-1;i>k;i--){
+                if(nums[i]> nums[k])
+                    break;   
             }
-            swap(nums[k], nums[l]);
+            swap(nums[i], nums[k]);
             reverse(nums.begin()+k+1, nums.end());
         }
+            
     }
 };
