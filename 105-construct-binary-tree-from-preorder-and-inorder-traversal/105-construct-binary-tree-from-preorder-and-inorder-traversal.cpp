f65429230b99e8be021607f29cf04462edaac098
Ankit Sagar <sagarankit13@gmail.com>
2022-07-14 12:31:21 +0530

    Time: 25 ms (74.09%), Space: 26.4 MB (47.35%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,29 +11,25 @@
  */
 class Solution {
 public:
-    TreeNode* build(vector<int>& preorder, int preStart, int preEnd, vector<int>& inorder, int inStart, int inEnd, map<int, int> &inMap){
-        if(preStart > preEnd || inStart > inEnd)
+    TreeNode* build(vector<int>&pre, int preStart, int preEnd, vector<int>&ino, int inStart, int inEnd, unordered_map<int, int>& inMap){
+        if(preStart> preEnd || inStart > inEnd)
             return NULL;
-        TreeNode* root = new TreeNode(preorder[preStart]);
+        TreeNode *root = new TreeNode(pre[preStart]);
         
         int inRoot = inMap[root->val];
         int numsLeft = inRoot - inStart;
         
-        root->left = build(preorder, preStart+1, preStart + numsLeft, inorder , inStart, inRoot - 1, inMap);
-        root->right = build(preorder, preStart + numsLeft+1, preEnd, inorder, inRoot +1, inEnd, inMap);
-        
+        root->left = build(pre, preStart+1, preStart+numsLeft, ino, inStart, inRoot-1, inMap);
+        root->right = build(pre, preStart+numsLeft+1, preEnd, ino, inRoot+1, inEnd, inMap);
         return root;
     }
     
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        map<int, int> inMap;
-        for(int i = 0;i<inorder.size();i++){
-            inMap[inorder[i]] = i;
+        unordered_map<int, int>inMap;
+        for(int i=0;i<inorder.size();++i){
+            inMap[inorder[i]] = i;   
         }
-        for(auto it:inMap){
-            cout << it.first << " " << it.second << endl;
-        }
-        TreeNode* newroot = build(preorder, 0, preorder.size()-1, inorder, 0,  inorder.size()-1, inMap);
-        return newroot;
+        TreeNode * root = build(preorder, 0, preorder.size()-1,inorder,0,inorder.size()-1,inMap);
+        return root;
     }
 };
