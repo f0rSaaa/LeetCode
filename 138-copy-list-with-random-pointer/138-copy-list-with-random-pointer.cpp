4c0b1aa32eecb828c44c7a9995b4e3a2f12e3675
Ankit Sagar <sagarankit13@gmail.com>
2022-07-14 21:33:31 +0530

    Time: 23 ms (9.36%), Space: 11.3 MB (27.11%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -17,21 +17,55 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
+        // if(head == NULL)
+        //     return NULL;
+        // unordered_map<Node* , Node*>mpp;
+        // Node* iter = head;
+        // while(iter != NULL){
+        //     Node * curr = new Node(iter->val);
+        //     mpp[iter] = curr;
+        //     iter = iter->next;
+        // }
+        // iter = head;
+        // while(iter != NULL){
+        //     mpp[iter]->next = mpp[iter->next];
+        //     mpp[iter]->random = mpp[iter->random];
+        //     iter=  iter->next;
+        // }
+        // return mpp[head];
+        
         if(head == NULL)
             return NULL;
-        unordered_map<Node* , Node*>mpp;
-        Node* iter = head;
+        Node* iter = head, *front = head;
+        //1st step create DC node right after original node
         while(iter != NULL){
-            Node * curr = new Node(iter->val);
-            mpp[iter] = curr;
-            iter = iter->next;
+            front = iter->next;
+            Node* copyNode = new Node(iter->val);
+            iter->next =copyNode;
+            copyNode->next = front;
+            iter = front;
         }
+        //2nd step assign random pointer
         iter = head;
-        while(iter != NULL){
-            mpp[iter]->next = mpp[iter->next];
-            mpp[iter]->random = mpp[iter->random];
-            iter=  iter->next;
+        while(iter!= NULL){
+            if(iter->random != NULL){
+                iter->next->random = iter->random->next;
+            }
+            iter = iter->next->next;
         }
-        return mpp[head];
+        //3rd step recover both Linked list OG and DC
+        iter= head;
+        Node* dummy = new Node(0);
+        dummy->next = iter->next;
+        Node * copyPtr = dummy;
+        while(iter != NULL){
+            front = iter->next->next;
+            copyPtr->next = iter->next;
+            iter->next = front;
+            copyPtr = copyPtr->next;
+            iter = front;
+        }
+        return dummy->next;
+        
     }
 };
