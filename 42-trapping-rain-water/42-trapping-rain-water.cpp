52901989e565f86aef5a5ed907b58173f1097d68
Ankit Sagar <sagarankit13@gmail.com>
2022-07-16 10:42:25 +0530

    Time: 19 ms (76.34%), Space: 20.9 MB (5.87%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -5,19 +5,19 @@ public:
         vector<int>left(n), right(n), water(n);
         left[0] = height[0];
         right[n-1] = height[n-1];
-        for(int i=1;i<n;i++){
+        for(int i=1;i<n;++i){
             left[i]= max(left[i-1], height[i]);
         }
         // for(auto it:left)
         //     cout << it << " ";
         // cout << endl;
-        for(int i= n-2;i>=0;i--){
+        for(int i= n-2;i>=0;--i){
             right[i] = max(right[i+1], height[i]);
         }
         // for(auto it:right)
         //     cout << it << " ";
         // cout << endl;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;++i){
             water[i] = min(left[i],right[i]) - height[i];
         }
         // for(auto it:water){
