ba7a57575b3fe3b8fd90e46cb630ce67941ffaec
Ankit Sagar <sagarankit13@gmail.com>
2022-07-16 11:35:29 +0530

    Time: 4 ms (63.03%), Space: 6.9 MB (26.94%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxPower(string s) {
+        int i =0;
+        int j;
+        int n = s.length();
+        int res=0;
+        while(i<n){
+            j = i+1;
+            while(j<n && s[j]== s[j-1]){
+                j++;
+            }
+            res = max(res, j-i);
+            ++i;
+        }
+        return res;
+    }
+};
