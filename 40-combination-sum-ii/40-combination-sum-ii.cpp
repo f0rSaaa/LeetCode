ca6bcecd7bf0b7326bd19924ff8f03492c27ad18
Ankit Sagar <sagarankit13@gmail.com>
2022-07-17 17:21:05 +0530

    Time: 12 ms (44.02%), Space: 10.6 MB (72.20%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    void solve(vector<int>&candidates,int target, vector<vector<int>>&ans, vector<int>&temp, int idx){
-        if(target == 0){
-            ans.push_back(temp);
-            return;
-        }
-        for(int i=idx;i<candidates.size();i++){
-            if(candidates[i]>target)
-                return;
-            if(i > idx && candidates[i] == candidates[i-1])
-                continue;
-            temp.push_back(candidates[i]);
-            solve(candidates,target-candidates[i],ans, temp,i+1);
-            temp.pop_back();
-        }
-    }
+   void solve(int idx, int tar, vector<int>&temp, vector<vector<int>>&res, vector<int>&can){
+       if(tar == 0){
+           res.push_back(temp);
+           return;
+       }
+       for(int i= idx;i<can.size() && can[i] <= tar; ++i){
+           if(i > idx && can[i] == can[i-1])
+               continue;
+           temp.push_back(can[i]);
+           solve(i+1,tar-can[i],temp, res, can);
+           temp.pop_back();
+       }
+   } 
     
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
-        vector<vector<int>>ans;
+    vector<vector<int>> combinationSum2(vector<int>& can, int tar) {
+        sort(can.begin(), can.end());
+        vector<vector<int>>res;
         vector<int>temp;
-        solve(candidates, target,ans,temp,0);
-        return ans;
+        solve(0,tar,temp,res,can);
+        return res;
     }
 };
