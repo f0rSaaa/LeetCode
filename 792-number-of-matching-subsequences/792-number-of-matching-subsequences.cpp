04757bd7261305b45601ce9859284e5e9da8caf5
Ankit Sagar <sagarankit13@gmail.com>
2022-07-20 07:56:02 +0530

    Time: 320 ms (61.14%), Space: 39.7 MB (83.90%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,50 +1,29 @@
 class Solution {
 public:
-//     bool isSub(string s, string& t){     //t big string s small string
-//         int n = s.size();
-//         int m = t.size();
-//         int i=0;
-//         int j=0;
-//         while(j < m){
-//             if(s[i] == t[j]){
-//                 ++i;
-//                 ++j;
-//             }else
-//                 ++j;
-//         }
-//         return (i ==n);
-//     }
-    
     int numMatchingSubseq(string s, vector<string>& words) {
-//         unordered_map<string, int>mpp;
-//         for(auto it:words){
-//             mpp[it]++;
-//         }
-//         int res = 0;
-//         for(auto it:mpp){
-//             if(isSub(it.first,s))
-//                 res+=it.second;
-//         }
-//         return res;
-//     }
-     int count=0,k=s.size();
-      //we will be form map to count occurences of particular word to avoid re calculation and this will reduce time
-      unordered_map<string,int>mymap;
-      for(int i=0;i<words.size();i++)
-        mymap[words[i]]++;
-      for(auto x:mymap)
-      {
-        int start1=0,start2=0;
-        string p=x.first;
-        int m=p.size();
-        while(start1<k&&start2<m)
-        {
-          if(p[start2]==s[start1])
-            start1++,start2++;
-          else
-            start1++;
-        }if(start2==m)
-          count+=x.second;//if we reach the length of string p then this word in in our string s. so we increase our count by number of times it occured in words instead of increamenting by 1
-      }return count;
+        unordered_map<string, int>mpp;
+        for(auto it:words){
+            mpp[it]++;
+        }
+        int res = 0;
+        int m = s.size();
+        for(auto it:mpp){
+            int i =0;
+            int j =0;
+            int n = it.first.size();
+            string t = it.first;
+            while(i < n && j < m){
+                if(t[i] == s[j]){
+                    ++i;
+                    ++j;
+                } else {
+                    ++j;
+                }
+            }
+            if(i == n)
+                res +=it.second;
+        }
+        return res;
     }
+
 };
