d217af3b04e17b99899d3ee6bc0176501ef7d3ac
Ankit Sagar <sagarankit13@gmail.com>
2022-07-20 07:57:00 +0530

    Time: 231 ms (82.23%), Space: 39.7 MB (84.27%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
+    bool isSub(string s, string& t){     //t big string s small string
+        int n = s.size();
+        int m = t.size();
+        int i=0;
+        int j=0;
+        while(i < n && j < m){
+            if(s[i] == t[j]){
+                ++i;
+                ++j;
+            }else
+                ++j;
+        }
+        if(i == n)
+            return true;
+        return false;
+    }
+    
     int numMatchingSubseq(string s, vector<string>& words) {
         unordered_map<string, int>mpp;
         for(auto it:words){
             mpp[it]++;
         }
         int res = 0;
-        int m = s.size();
         for(auto it:mpp){
-            int i =0;
-            int j =0;
-            int n = it.first.size();
-            string t = it.first;
-            while(i < n && j < m){
-                if(t[i] == s[j]){
-                    ++i;
-                    ++j;
-                } else {
-                    ++j;
-                }
-            }
-            if(i == n)
-                res +=it.second;
+            if(isSub(it.first,s))
+                res+=it.second;
         }
         return res;
     }
-
 };
