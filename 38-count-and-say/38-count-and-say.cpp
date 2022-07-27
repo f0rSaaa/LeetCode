016e82c2715ffa6260cbfa7c6c0d1b1348d8553a
Ankit Sagar <sagarankit13@gmail.com>
2022-07-27 21:14:53 +0530

    Time: 15 ms (44.51%), Space: 6.6 MB (55.61%) - LeetHub

--- 38-count-and-say/38-count-and-say.cpp
+++ 38-count-and-say/38-count-and-say.cpp
@@ -6,17 +6,17 @@ public:
         if(n == 2)
             return "11";
         string s = "11";
-        for(int i=3;i<=n;i++){
-            string temp = "";
+        for(int i=3;i<=n;++i){
+            string temp ="";
+            s+='#';
+            int m= s.size();
             int count = 1;
-            s+='#';               //to denote the end of the string
-            int m = s.length();
-            for(int j=1;j<m;j++){
+            for(int j=1;j<m;++j){
                 if(s[j] == s[j-1])
                     count++;
                 else {
-                    temp+=to_string(count);
-                    temp+=s[j-1];
+                    temp += to_string(count);
+                    temp += s[j-1];
                     count = 1;
                 }
             }
